c5768e44e8ac811320da6a35fe8f803c82f8fe05
flinco <francodeciiudadela@gmail.com>
2023-07-13 19:51:27 -0300

    Add flash

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import { PORT } from "./config.js";
 import morgan from "morgan";
 
+import flash from "connect-flash";
+import session from "express-session";
+
 import handlebars from "express-handlebars";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
@@ -11,13 +14,15 @@ import Routerlogin from "./routes/auth.routes.js";
 
 //Iniciamos Express
 const app = express();
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
+//Configuramos path para que sea mas comodo
+const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set("views", join(__dirname, "views"));
 
 //Configuramos handlebars
 const hbs = handlebars.create({
   layoutsDir: join(app.get("views"), "layouts"),
+  partialsDir: join(app.get("views"), "partials"),
   extname: ".hbs",
 });
 app.engine(".hbs", hbs.engine);
@@ -29,6 +34,23 @@ app.use(morgan("dev"));
 //Establece el port de la variable de entorno
 app.set("port", PORT);
 
+//Mensajes flash y Express-session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
+//Variables Globales
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  next();
+});
+
+//Routes
 app.use(Routerindex);
 app.use(Routerlogin);
 
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 export const renderSignUpForm = (req, res) => res.render("auth/signup");
 
-/*export const signup = (req, res) => {
-  let errors = [];
-  const {} = req.body
-};*/
+export const signup = (req, res) => {
+  const { name } = req.body;
+  console.log("este es el :", name);
+  req.flash("success_msg", "You are registered.");
+  return res.redirect("/");
+};
